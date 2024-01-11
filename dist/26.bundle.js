9fecadf90f20612ab76060ffb951c8fbe50b3b2f
OmarMWarraich <earthling986@gmail.com>
2024-01-11 19:13:34 +0500

    feat: Starter Code

--- dist/26.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktodo_list=self.webpackChunktodo_list||[]).push([[26],{7026:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { createClient } from '@supabase/supabase-js';
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  progressConversation();
+});
 
-try {
-  const result = await fetch('../scrimba-info.txt');
-  const text = await result.text();
+const openAIApiKey = process.env.OPENAI_API_KEY;
 
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    separators: ['\n\n', '\n', ' ', ''],
-    chunkOverlap: 50,
-  });
+async function progressConversation() {
+  const userInput = document.getElementById('user-input');
+  const chatbotConversation = document.getElementById('chatbot-conversation-container');
+  const question = userInput.value;
+  userInput.value = '';
 
-  const output = await splitter.createDocuments([text]);
+  // add human message
+  const newHumanSpeechBubble = document.createElement('div');
+  newHumanSpeechBubble.classList.add('speech', 'speech-human');
+  chatbotConversation.appendChild(newHumanSpeechBubble);
+  newHumanSpeechBubble.textContent = question;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 
-  const sbApiKey = process.env.SUPABASE_API_KEY;
-  const sbUrl = process.env.SUPABASE_URL;
-  const openAIApiKey = process.env.OPENAI_API_KEY;
-
-  const client = createClient(sbUrl, sbApiKey);
-
-  await SupabaseVectorStore.fromDocuments(
-    output,
-    new OpenAIEmbeddings({ openAIApiKey }),
-    {
-      client,
-      tableName: 'documents',
-    },
-  );
-} catch (error) {
-  console.log(error);
+  // add AI message
+  const newAiSpeechBubble = document.createElement('div');
+  newAiSpeechBubble.classList.add('speech', 'speech-ai');
+  chatbotConversation.appendChild(newAiSpeechBubble);
+  newAiSpeechBubble.textContent = result;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 }
